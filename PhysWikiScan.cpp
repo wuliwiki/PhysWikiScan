3f034ceb52bcde7a80f4625f0c0ad228ef8e84f8
MacroUniverse <270174408@qq.com>
2017-12-11 17:03:12 -0600

    found normal text

--- PhysWikiScan.cpp
+++ PhysWikiScan.cpp
@@ -90,7 +90,7 @@ void WriteUTF8(CString text, CString path)
 }
 
 // see if a key appears followed only with only white space
-// return the next index if yes, return -1 if no.
+// return the next index, return -1 if nothing found.
 int ExpectKey(const CString& str, CString key, int start)
 {
 	int ind = start;
@@ -119,18 +119,20 @@ int ExpectKey(const CString& str, CString key, int start)
 // find a scope in str for environment named env
 // output: ind is index in pairs
 // return number of environments found. return -1 if failed
-int FindEnv(vector<int>& ind, const CString& str, CString env)
+// if option = 'i', range starts from the next index of \begin{} and previous index of \end{}
+// if option = 'o', range starts from '\' of \begin{} and '}' of \end{}
+int FindEnv(vector<int>& ind, const CString& str, CString env, char option = 'i')
 {
-	int ind0 = 0, i, ind1, ind2;
+	int ind0 = 0, i, ind1, ind2, ind3;
 	int N{}; // number of environments found
 	ind.resize(0);
 	while (true)
 	{
 		// find "\\begin"
-		ind0 = str.Find(_T("\\begin"), ind0);
-		if (ind0 < 0)
+		ind3 = str.Find(_T("\\begin"), ind0);
+		if (ind3 < 0)
 			return N;
-		ind0 += 6;
+		ind0 = ind3 + 6;
 
 		// find '{'
 		ind0 = ExpectKey(str, _T("{"), ind0);
@@ -145,7 +147,7 @@ int FindEnv(vector<int>& ind, const CString& str, CString env)
 		ind0 = ExpectKey(str, _T("}"), ind0);
 		if (ind0 < 0)
 			return -1;
-		ind.push_back(ind0);
+		ind.push_back(option == 'i'? ind0 : ind3);
 
 		while (true)
 		{
@@ -168,7 +170,7 @@ int FindEnv(vector<int>& ind, const CString& str, CString env)
 			ind0 = ExpectKey(str, _T("}"), ind0);
 			if (ind0 < 0)
 				return -1;
-			ind.push_back(ind2);
+			ind.push_back(option == 'i'? ind2 : ind0 - 1);
 			++N;
 			break;
 		}
@@ -197,10 +199,11 @@ int FindInline0(vector<int>& ind, const CString& str, int begin, int end)
 	return N / 2;
 }
 
-// find inline equations using $$
+// find the range of inline equations using $$
+// if option = 'i', range does not include $, if 'o', it does.
 // need the result from equation environment
 // return the number of $$ environments found.
-int FindInline(vector<int>& ind, const CString& str, vector<int>& indEq)
+int FindInline(vector<int>& ind, const CString& str, vector<int>& indEq, char option = 'i')
 {
 	ind.resize(0);
 	int N{}; // number of $$
@@ -219,6 +222,14 @@ int FindInline(vector<int>& ind, const CString& str, vector<int>& indEq)
 		for (int i = 1; i < Neq * 2 - 1; i += 2)
 			N += FindInline0(ind, str, indEq[i], indEq[i + 1]);
 		N += FindInline0(ind, str, indEq.back(), str.GetLength() - 1);
+	}
+
+	if (option == 'i' && N > 0)
+	{
+		for (int i{}; i < 2 * N; i += 2)
+			ind[i] += 1;
+		for (int i{ 1 }; i < 2 * N; i += 2)
+			ind[i] -= 1;
 	}
 	return N;
 }
@@ -246,27 +257,28 @@ int PairBraceR(const CString& str, int ind)
 	return -1;
 }
 
-// Find all \texttt{} environment in str
-// index pairs of \texttt scope (ind).
-// return number of \texttt{} found, return -1 if failed.
-int FindTT(vector<int>& ind, CString& str)
+// Find all <key>{} environment in str
+// output ranges to ind
+// if option = 'i', range does not include {}, if 'o', range from first letter of <key> to '}'
+// return number of <key>{} found, return -1 if failed.
+int FindComBrace(vector<int>& ind, const CString& key, const CString& str, char option = 'i')
 {
 	ind.resize(0);
-	int ind0{};
+	int ind0{}, ind1;
 	while (true)
 	{
-		ind0 = str.Find(_T("\texttt"));
-		if (ind0 < 0)
+		ind1 = str.Find(key, ind0);
+		if (ind1 < 0)
 			return ind.size() / 2;
-		ind0 += 7;
+		ind0 = ind1 + key.GetLength();
 		ind0 = ExpectKey(str, _T("{"), ind0);
 		if (ind0 < 0)
 			return -1;
-		ind.push_back(ind0);
+		ind.push_back(option == 'i'? ind0 : ind1);
 		ind0 = PairBraceR(str, ind0 - 1);
 		if (ind0 < 0)
-			cout << "error!"; return -1;
-		ind.push_back(ind0 - 1);
+			{ cout << "error!"; return -1; }
+		ind.push_back(option == 'i'? ind0 - 1 : ind0);
 	}
 }
 
@@ -300,12 +312,13 @@ void sort(vector<int>& x, vector<int>& ind)
 	}
 }
 
-// combine scopes ind1 and ind1
-// ind1 or ind 2 must be be in order and not overlap with themselves
-// ind1 can overlap ind2
-int CombineScope(vector<int>& ind, vector<int> ind1, vector<int> ind2)
+// combine ranges ind1 and ind2
+// no range in ind1 or ind2 should overlap with others.
+// return total range number, or -1 if failed.
+int CombineRange(vector<int>& ind, vector<int> ind1, vector<int> ind2)
 {
 	int i, N1 = ind1.size(), N2 = ind2.size();
+	// load start and end, and sort
 	vector<int> start, end, order;
 	for (i = 0; i < N1; i += 2)
 		start.push_back(ind1[i]);
@@ -322,12 +335,17 @@ int CombineScope(vector<int>& ind, vector<int> ind1, vector<int> ind2)
 	for (i = 0; i < N; ++i)
 		temp[i] = end[order[i]];
 	end = temp;
+	
+	// load ind
+	ind.resize(0);
 	ind.push_back(start[0]);
 	i = 0;
 	while(i < N - 1)
 	{
 		if (end[i] >= start[i + 1])
-		{ ++i; continue; }
+			{ cout << "error! range overlap!"; return -1; }
+		else if (end[i] == start[i+1] - 1)
+			{ ++i; continue; }
 		else
 		{
 			ind.push_back(end[i]);
@@ -338,10 +356,66 @@ int CombineScope(vector<int>& ind, vector<int> ind1, vector<int> ind2)
 	ind.push_back(end.back());
 }
 
-// Find normal text
-int FindNormalText(vector<int>& ind, CString& str)
+// invert ranges in ind0, output to ind1
+int InvertRange(vector<int>& ind, const vector<int>& ind0, int Nstr)
 {
+	ind.resize(0);
+	if (ind0.size() == 0)
+		{ ind.push_back(0); ind.push_back(Nstr-1); return 1; }
 
+	int N{}; // total num of ranges output
+	if (ind0[0] > 0)
+		{ ind.push_back(0); ind.push_back(ind0[0] - 1); ++N; }
+	for (int i{ 1 }; i < ind0.size() - 1; i += 2)
+	{
+		ind.push_back(ind0[i] + 1);
+		ind.push_back(ind0[i + 1] - 1);
+		++N;
+	}
+	if (ind0.back() < Nstr - 1)
+		{ ind.push_back(ind0.back() + 1); ind.push_back(Nstr - 1); ++N; }
+	return N;
+}
+
+// Find normal text range
+int FindNormalText(vector<int>& indNorm, CString& str)
+{
+	vector<int> ind, indEq, indInline, indCom, indGath, indAli, indTT, indFig, indInp;
+	int NEq, NInline, NCom, NGath, NAli, NTT, NFig, NInp;
+	// inline equation environments
+	NInline = FindInline(indInline, str, indEq, 'o');
+	ind = indInline;
+	// equation environments
+	NEq = FindEnv(indEq, str, _T("equation"), 'o');
+	if (NEq > 0)
+		CombineRange(ind, ind, indEq);
+	// command environments
+	NCom = FindEnv(indCom, str, _T("Command"), 'o');
+	if (NCom > 0)
+		CombineRange(ind, ind, indCom);
+	// gather environments
+	NGath = FindEnv(indGath, str, _T("gather"), 'o');
+	if (NGath > 0)
+		CombineRange(ind, ind, indGath);
+	// align environments (not "aligned")
+	NAli = FindEnv(indAli, str, _T("align"), 'o');
+	if (NAli > 0)
+		CombineRange(ind, ind, indAli);
+	// texttt command
+	NTT = FindComBrace(indTT, _T("\\texttt"), str, 'o');
+	if (NTT > 0)
+		CombineRange(ind, ind, indTT);
+	// input command
+	NInp = FindComBrace(indInp, _T("\\input"), str, 'o');
+	if (NInp > 0)
+		CombineRange(ind, ind, indInp);
+	// Figure environments
+	NFig = FindEnv(indFig, str, _T("figure"), 'o');
+	if (NFig > 0)
+		CombineRange(ind, ind, indFig);
+
+	// invert range
+	return InvertRange(indNorm, ind, str.GetLength());
 }
 
 // match braces
@@ -448,7 +522,7 @@ int OneFile2(CString path)
 {
 	CString str = ReadUTF8(path); // read file
 
-								  // get all the equation environment scopes
+	// get all the equation environment scopes
 	vector<int> indEq;
 	if (FindEnv(indEq, str, _T("equation")) < 0)
 	{
@@ -475,9 +549,42 @@ int OneFile2(CString path)
 	return N;
 }
 
+// remove any nonchinese punctuations from normal text
+int OneFile3(CString path)
+{
+	int NNorm, N{};
+	vector<int> indNorm;
+	CString str = ReadUTF8(path); // read file
+	NNorm = FindNormalText(indNorm, str);
+
+	// test the result of indNorm
+	/*for (int i{ NNorm*2-1 }; i >= 0; i -= 2)
+	{
+		str.Insert(indNorm[i] + 1, _T("正文结束"));
+		str.Insert(indNorm[i-1], _T("正文开始"));
+		++N;
+	}*/
+
+	// search nonchinese punctuations
+	TCHAR c;
+	for (int i{ NNorm * 2 - 1 }; i >= 0; i -= 2)
+	{
+		for (int j{ indNorm[i] }; j >= indNorm[i - 1]; --j)
+		{
+			c = str[j];
+			if (c == '(' || c == ')' || c == ',' || c == '.' || c == '?' || c == ':' || c == '!')
+				{ str.Insert(j, _T("删除标记")); ++N; }
+		}
+	}
+
+	if (N > 0)
+		WriteUTF8(str, path);
+	return N;
+}
+
 void main()
 {
-	CString path0 = _T("C:\\Users\\addis\\Documents\\GitHub\\PhysWiki\\contents\\");
+	CString path0 = _T("C:\\Users\\addis\\Desktop\\");
 	//_T("C:\\Users\\addis\\Documents\\GitHub\\PhysWiki\\contents\\");
 	//_T("C:\\Users\\addis\\Desktop\\");
 	vector<CString> names = GetFileNames(path0, _T("tex"));
@@ -485,7 +592,7 @@ void main()
 	for (int i{}; i < names.size(); ++i)
 	{
 		wcout << names[i].GetString() << _T("...");
-		N = OneFile2(path0 + names[i]);
+		N = OneFile3(path0 + names[i]);
 		cout << N << endl;
 	}
 }
