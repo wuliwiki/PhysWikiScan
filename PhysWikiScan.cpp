85af2a63471c580242403eb96d9e02d4a2f598c7
MacroUniverse <270174408@qq.com>
2018-02-09 02:58:13 -0600

    html

--- PhysWikiScan.cpp
+++ PhysWikiScan.cpp
@@ -69,7 +69,7 @@ CString ReadUTF8(CString path)
 	fin.open(path, ios::in);
 	if (!fin.is_open())
 	{
-		printf("open input file error\n");
+		wcout << _T("open input file error");
 		return _T("error");
 	}
 	memset(buffer, 0, 1024 * 1024);
@@ -89,6 +89,90 @@ void WriteUTF8(CString text, CString path)
 		printf("open output file error\n");
 	fout.write(out_text.c_str(), out_text.size());
 	fout.close();
+}
+
+// see if an index i falls into the scopes of ind
+bool IndexInRange(int i, vector<int> ind)
+{
+	if (ind.size() == 0)
+		return false;
+	for (unsigned j{}; j < ind.size() - 1; j += 2) {
+		if (ind[j] <= i && i <= ind[j + 1])
+			return true;
+	}
+	return false;
+}
+
+// find comments
+// output: ind is index in pairs
+// return number of comments found. return -1 if failed
+// range is from '%' to 'CR'
+int FindComment(vector<int>& ind, const CString& str)
+{
+	int ind0{}, ind1{};
+	int N{}; // number of comments found
+	ind.resize(0);
+	//TCHAR escape{'\\'};
+	while (true) {
+		ind1 = str.Find(_T("%"), ind0);
+		if (ind1 >= 0)
+			if (ind1 == 0 || (ind1 > 0 && str.GetAt(ind1 - 1) != '\\')) {
+				ind.push_back(ind1); ++N;
+			}
+			else {
+				ind0 = ind1 + 1;  continue;
+			}
+		else
+			return N;
+
+		ind1 = str.Find(_T("\n"), ind1 + 1);
+		if (ind1 < 0) {// not found
+			ind.push_back(str.GetLength() - 1);
+			return N;
+		}
+		else
+			ind.push_back(ind1);
+
+		ind0 = ind1;
+	}
+}
+
+// find next character that is not a space
+// output single character c, return the position of the c
+// return -1 if not found
+int NextNoSpace(CString& c, const CString& str, int start)
+{
+	for (int i{ start }; i < (int)str.GetLength(); ++i) {
+		c = str.GetAt(i);
+		if (c == ' ')
+			continue;
+		else
+			return i;
+	}
+	return -1;
+}
+
+// get the title (defined as the first comment, no space after %)
+// limited to 20 characters
+// return -1 if error, 0 if successful
+int GetTitle(CString& title, const CString& str)
+{
+	if (str.GetAt(0) != '%') {
+		wcout << _T("Need a title!");
+		return -1;
+	}
+	CString c;
+	int ind0 = NextNoSpace(c, str, 1);
+	if (ind0 < 0) {
+		wcout << _T("Title is empty!");
+		return -1;
+	}
+	int ind1 = str.Find('\n', ind0);
+	if (ind1 < 0) {
+		wcout << _T("Body is empty!");
+	}
+	title = str.Mid(ind0, ind1 - ind0);
+	return 0;
 }
 
 // see if a key appears followed only with only white space
@@ -126,26 +210,28 @@ int ExpectKey(const CString& str, CString key, int start)
 int FindEnv(vector<int>& ind, const CString& str, CString env, char option = 'i')
 {
 	int ind0{}, ind1{}, ind2{}, ind3{};
+	vector<int> indComm{}; // result from FindComment
 	int N{}; // number of environments found
 	ind.resize(0);
-	while (true)
-	{
+	FindComment(indComm, str);
+	while (true) {
 		// find "\\begin"
 		ind3 = str.Find(_T("\\begin"), ind0);
+		if (IndexInRange(ind3, indComm)) { ind0 = ind3 + 6; continue; }
 		if (ind3 < 0)
 			return N;
 		ind0 = ind3 + 6;
 
-		// find '{'
+		// expect '{'
 		ind0 = ExpectKey(str, _T("{"), ind0);
 		if (ind0 < 0)
 			return -1;
-		// find 'env'
+		// expect 'env'
 		ind1 = ExpectKey(str, env, ind0);
 		if (ind1 < 0)
 			continue;
 		ind0 = ind1;
-		// find '}'
+		// expect '}'
 		ind0 = ExpectKey(str, _T("}"), ind0);
 		if (ind0 < 0)
 			return -1;
@@ -155,20 +241,21 @@ int FindEnv(vector<int>& ind, const CString& str, CString env, char option = 'i'
 		{
 			// find "\\end"
 			ind0 = str.Find(_T("\\end"), ind0);
+			if (IndexInRange(ind0, indComm)) { ind0 += 4; continue; }
 			if (ind0 < 0)
 				return -1;
 			ind2 = ind0 - 1;
 			ind0 += 4;
-			// find '{'
+			// expect '{'
 			ind0 = ExpectKey(str, _T("{"), ind0);
 			if (ind0 < 1)
 				return -1;
-			// find 'env'
+			// expect 'env'
 			ind1 = ExpectKey(str, env, ind0);
 			if (ind1 < 0)
 				continue;
 			ind0 = ind1;
-			// find '}'
+			// expect '}'
 			ind0 = ExpectKey(str, _T("}"), ind0);
 			if (ind0 < 0)
 				return -1;
@@ -179,60 +266,30 @@ int FindEnv(vector<int>& ind, const CString& str, CString env, char option = 'i'
 	}
 }
 
-// find inline equations using $$
-// indices of every $ appended to ind.
-// return number of $$ environments found
-int FindInline0(vector<int>& ind, const CString& str, int begin, int end)
-{
-	int i, N{};
-	TCHAR c, c0 = ' ';
-	for (i = begin; i <= end; ++i)
-	{
-		c = str.GetAt(i);
-		if (c == '$' && c0 != '\\')
-		{
-			ind.push_back(i);
-			++N;
-		}
-		c0 = c;
-	}
-	if (N % 2 != 0)
-		return -1;
-	return N / 2;
-}
-
 // find the range of inline equations using $$
 // if option = 'i', range does not include $, if 'o', it does.
-// need the result from equation environment
 // return the number of $$ environments found.
-int FindInline(vector<int>& ind, const CString& str, vector<int>& indEq, char option = 'i')
+int FindInline(vector<int>& ind, const CString& str, char option = 'i')
 {
 	ind.resize(0);
 	int N{}; // number of $$
-	int Neq = indEq.size() / 2; // number of equation environments
-	if (Neq == 0)
-	{
-		N = FindInline0(ind, str, 0, str.GetLength() - 1);
-		if (N < 0)
-		{
-			wcout << L"error!"; return -1;
-		}
+	int ind0{};
+	vector<int> indComm; // result from FindComment
+	FindComment(indComm, str);
+	while (true) {
+		ind0 = str.Find(_T("$"), ind0);
+		if (ind0 < 0) { break; } // did not find
+		if (ind0 > 0 && str.GetAt(ind0 - 1) == '\\') { ++ind0; continue; } // not an escape
+		if (IndexInRange(ind0, indComm)) { ++ind0; continue; } // not in comment
+		ind.push_back(ind0);
+		++ind0; ++N;
 	}
-	else
-	{
-		N = FindInline0(ind, str, 0, indEq[0]);
-		for (int i = 1; i < Neq * 2 - 1; i += 2)
-			N += FindInline0(ind, str, indEq[i], indEq[i + 1]);
-		N += FindInline0(ind, str, indEq.back(), str.GetLength() - 1);
-	}
-
+	if (N % 2 != 0) return -1;// odd number of $ found
+	N /= 2;
 	if (option == 'i' && N > 0)
-	{
-		for (int i{}; i < 2 * N; i += 2)
-			ind[i] += 1;
-		for (int i{ 1 }; i < 2 * N; i += 2)
-			ind[i] -= 1;
-	}
+		for (int i{}; i < 2 * N; i += 2) {
+			++ind[i]; --ind[i + 1];
+		}
 	return N;
 }
 
@@ -284,7 +341,7 @@ int FindComBrace(vector<int>& ind, const CString& key, const CString& str, char 
 	}
 }
 
-// sort x in descending order while keeping the index
+// sort x in descending order while tracking the original index
 void sort(vector<int>& x, vector<int>& ind)
 {
 	int i, temp;
@@ -393,12 +450,16 @@ int InvertRange(vector<int>& ind, const vector<int>& ind0, int Nstr)
 // Find normal text range
 int FindNormalText(vector<int>& indNorm, CString& str)
 {
-	vector<int> ind, indEq, indInline, indCom, indGath, indAli, 
+	vector<int> ind, indComm, indEq, indInline, indCom, indGath, indAli, 
 		indTT, indFig, indInp, indTable, indSSub;
-	int NEq, NInline, NCom, NGath, NAli, NTT, NFig, NInp, NTable, NSSub;
+	int NComm, NEq, NInline, NCom, NGath, NAli, NTT, NFig, NInp, NTable, NSSub;
+	// comments
+	NComm = FindComment(indComm, str);
+	ind = indComm;
 	// inline equation environments
-	NInline = FindInline(indInline, str, indEq, 'o');
-	ind = indInline;
+	NInline = FindInline(indInline, str, 'o');
+	if (NInline > 0)
+		CombineRange(ind, ind, indInline);
 	// equation environments
 	NEq = FindEnv(indEq, str, _T("equation"), 'o');
 	if (NEq > 0)
@@ -510,6 +571,35 @@ int RemoveBraces(vector<int>& ind_left, vector<int>& ind_right,
 	return N;
 }
 
+// add <p> tags to paragraphs
+// return number of tag pairs added
+int ParagraphTag(CString& str)
+{
+	int i{}, N{}, ind0{};
+	vector<int> ind;
+	// delete extra '\n' (more than two continuous)
+	while (true) {
+		ind0 = str.Find(_T("\n\n\n"), ind0);
+		if (ind0 < 0) break;
+		str.Delete(ind0, 1);
+	}
+	// remove "\n\n" and add <p> tags
+	ind0 = 0;
+	while (true) {
+		ind0 = str.Find(_T("\n\n"), ind0);
+		if (ind0 < 0) break;
+		ind.push_back(ind0); ++N; ind0 += 2;
+	}
+	N = ind.size();
+	for (i = N - 2; i > 0; --i) {
+		str.Insert(ind[i] + 1, _T("<p>"));
+		str.Insert(ind[i], _T("\n</p>"));
+	}
+	str.Insert(str.GetLength() - 1, _T("</p>"));
+	str.Insert(0, _T("<p>"));
+	return N;
+}
+
 // remove extra {} from equation environment and rewrite file
 // return total {} deleted
 int OneFile1(CString path)
@@ -551,7 +641,7 @@ int OneFile2(CString path)
 		wcout << L"error!"; return 0;
 	}
 	vector<int> ind; // indices for all the $.
-	if (FindInline(ind, str, indEq) == 0)
+	if (FindInline(ind, str) == 0)
 		return 0;
 
 	// match  and remove braces
@@ -605,20 +695,72 @@ int OneFile3(CString path)
 	return N;
 }
 
+// generate html from tex
+// return 0 if successful, -1 if failed
+int tex2html1(CString path)
+{
+	// read template file from local folder
+	int i{}, N{}, ind0;
+	CString str = ReadUTF8(path); // read tex file
+	//// read template from the same folder as path
+	//ind0 = path.ReverseFind('\\');
+	//path.Delete(ind0, str.GetLength() - ind0);
+	//path += _T("\\template.html");
+	CString html = ReadUTF8(_T("template.html")); // read html template
+	CString newcomm = ReadUTF8(_T("newcommand.html"));
+	CString title; // chinese title of an entry
+	if (GetTitle(title, str) < 0) return -1;
+	// insert \newcommand
+	ind0 = html.Find(_T("PhysWikiCommand"), 0);
+	html.Delete(ind0, 15);
+	html.Insert(ind0, newcomm);
+	// insert HTML title
+	ind0 = html.Find(_T("PhysWikiHTMLtitle"), 0);
+	html.Delete(ind0, 17);
+	html.Insert(ind0, title);
+	// remove comments
+	vector<int> indComm;
+	N = FindComment(indComm, str);
+	for (i = 2 * N - 2; i >= 0; i -= 2) {
+		str.Delete(indComm[i], indComm[i + 1] - indComm[i] + 1);
+	}
+	// add paragraph tags
+	ParagraphTag(str);
+	wcout << endl << str.GetString() << endl;
+	// insert HTML body
+	ind0 = html.Find(_T("PhysWikiHTMLbody"), ind0);
+	html.Delete(ind0, 16);
+	html.Insert(ind0, str);
+	
+	
+	wcout << endl << html.GetString() << endl;
+
+	// save html file
+	path.Delete(path.GetLength() - 3, 3);
+	path.Insert(path.GetLength(), _T("html"));
+	WriteUTF8(html, path);
+	return 0;
+}
+
+
 void main()
 {
 	_setmode(_fileno(stdout), _O_U16TEXT); // for console unicode output
 
-	CString path0 = _T("C:\\Users\\addis\\Documents\\GitHub\\PhysWiki\\contents1\\");
-	//_T("C:\\Users\\addis\\Documents\\GitHub\\PhysWiki\\contents\\");
-	//_T("C:\\Users\\addis\\Desktop\\");
+	//CString path0 = _T("C:\\Users\\addis\\Documents\\GitHub\\PhysWiki\\contents\\");
+	//CString path0 = _T("C:\\Users\\addis\\Desktop\\");
+	CString path0 = _T("C:\\Users\\addis\\Documents\\GitHub\\littleshi.cn\\root\\PhysWiki\\online\\");
+
 	vector<CString> names = GetFileNames(path0, _T("tex"));
 	int N;
 	for (unsigned i{}; i < names.size(); ++i)
 	{
 		wcout << i << " ";
 		wcout << names[i].GetString() << _T("...");
-		N = OneFile2(path0 + names[i]);
-		wcout << N << endl;
+		//N = OneFile3(path0 + _T("BEng.tex"));
+		//N = OneFile3(path0 + names[i]);
+		//wcout << N << endl;
+
+		tex2html1(path0 + names[i]);
 	}
 }
