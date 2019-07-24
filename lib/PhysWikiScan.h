4505924254504ebf503f83ba1b70dc4ef7b01ecf
MacroUniverse <270174408@qq.com>
2019-07-24 11:45:43 -0500

    implement "textbf"

--- lib/PhysWikiScan.h
+++ lib/PhysWikiScan.h
@@ -1229,7 +1229,7 @@ inline Long PhysWikiOnline1(vector_IO<Str32> ids, vector_IO<Str32> labels, vecto
 	// replace \name{} with html tags
 	Command2Tag(U"subsection", U"<h5 class = \"w3-text-indigo\"><b>", U"</b></h5>", str);
 	Command2Tag(U"subsubsection", U"<h5><b>", U"</b></h5>", str);
-	Command2Tag(U"bb", U"<b>", U"</b>", str);
+	Command2Tag(U"bb", U"<b>", U"</b>", str); Command2Tag(U"textbf", U"<b>", U"</b>", str);
 	// replace \upref{} with link icon
 	if (upref(str, path_in) < 0)
 		return -1;
