913142d1ad16dfb5131bd39911b0f1509cff4045
MacroUniverse <270174408@qq.com>
2019-07-31 21:51:00 -0500

    fixed a bug about Matlab highlight

--- Matlab/matlab.h
+++ Matlab/matlab.h
@@ -3,13 +3,14 @@
 
 namespace slisc {
 
-// determin if a apostrophe is a transpose operator
+// determin if a apostrophe is a transpose operator (ind must not be inside string)
 inline Long Matlab_is_trans(Str32_I str, Long_I ind)
 {
 	if (str.at(ind) != U'\'')
 		SLS_ERR("not an apostrophe!");
 	Long ind0 = ind - 1;
-	if (ind0 >= 0 && (is_alphanum_(str[ind0]) || str[ind0] == U'.'))
+	if (ind0 >= 0 && (is_alphanum_(str[ind0]) || str[ind0] == U'.' ||
+		str[ind0] == U']' || str[ind0] == U')'))
 		return true;
 	return false;
 }
@@ -25,8 +26,9 @@ inline Long Matlab_comments(Intvs_O intv, Str32_I str, Intvs_I intv_str)
 		if (ind0 < 0) {
 			return intv.size();
 		}
-		if (is_in(ind0, intv_str))
-			continue;
+		if (is_in(ind0, intv_str)) {
+			++ind0;	continue;
+		}
 		intv.pushL(ind0);
 		ind0 = str.find(U'\n', ind0);
 		// last line, line ending
