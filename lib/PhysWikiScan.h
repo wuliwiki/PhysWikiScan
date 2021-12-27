48a02861b124636f4fd587e88e067a53e72ce461
MacroUniverse <270174408@qq.com>
2021-12-26 21:12:16 -0600

    force chinese punctuations

--- lib/PhysWikiScan.h
+++ lib/PhysWikiScan.h
@@ -1863,7 +1863,7 @@ inline Long PhysWikiOnline1(vecStr32_IO ids, vecStr32_IO labels, vecLong_IO link
     // forbid empty lines in equations
     check_eq_empty_line(str);
     // check english puctuation in normal text
-    if (gv::eng_punc)
+    if (gv::is_wiki || gv::eng_punc)
         check_normal_text_punc(str, true);
     // add spaces around inline equation
     inline_eq_space(str);
