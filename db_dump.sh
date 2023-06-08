1eca0fdf6c21cd1f693cc3a79862334b891cd97e
MacroUniverse <270174408@qq.com>
2023-06-07 19:51:47 -0500

    trivial

--- db_dump.sh
+++ db_dump.sh
@@ -1,3 +1,1 @@
 sqlite3 data/scan.db .dump > data/scan.sql
-chmod 664 data/scan.sql
-chgrp wiki data/scan.sql
--- db_restore.sh
+++ db_restore.sh
@@ -1,4 +1,2 @@
 rm -f data/scan.db
 sqlite3 data/scan.db < data/scan.sql
-chmod 664 data/scan.db
-chgrp wiki data/scan.db
