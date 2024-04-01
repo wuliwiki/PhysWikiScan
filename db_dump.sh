dbdc4a0f0baab3c76fd7b6ddf46e4dc9ed904acf
MacroUniverse <270174408@qq.com>
2024-04-01 13:05:30 +0800

    amend

--- db_dump.sh
+++ db_dump.sh
@@ -3,3 +3,9 @@ cp data/scan.sql data/scan-$(date +'%Y%m%d.%H%M%S').sql
 
 python3 data/sqlite-reorder.py data/scan.sql
 cp data/stable-scan.sql data/stable-scan-$(date +'%Y%m%d.%H%M%S').sql
+
+lines1=$(wc -l < "data/scan.sql")
+lines2=$(wc -l < "data/stable-scan.sql")
+if [ "$lines1" -ne "$lines2" ]; then
+  echo "==== ERROR: reordered sql has different # of lines! ===="
+fi
