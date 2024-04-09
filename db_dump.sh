162c81cf507367b7b96d95397f604052ffe0252e
MacroUniverse <270174408@qq.com>
2024-04-09 10:59:05 -0500

    mv to MyPythonLibrary

--- db_dump.sh
+++ db_dump.sh
@@ -1,7 +1,7 @@
 sqlite3 data/scan.db .dump > data/scan.sql
 cp data/scan.sql data/scan-$(date +'%Y%m%d.%H%M%S').sql
 
-python3 data/sqlite-reorder.py data/scan.sql
+sql-stab.py data/scan.sql
 cp data/stable-scan.sql data/stable-scan-$(date +'%Y%m%d.%H%M%S').sql
 
 lines1=$(wc -l < "data/scan.sql")
