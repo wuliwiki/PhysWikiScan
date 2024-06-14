4cd9093b2f73a82ee756b3b4a0bf8b58fba05efa
MacroUniverse <270174408@qq.com>
2024-06-15 02:05:18 +0800

    db_dump service

---
+++ db_auto_dump.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# usage:
+#     sudo nohup ./sync.sh >> sync.log &
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+while true; do
+    date
+    echo -----------------------------------
+    ./db_dump.sh
+    printf "sleeping...\n\n\n"
+    sleep 12h
+done
